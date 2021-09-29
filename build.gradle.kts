cafa6e28e6e4d441a7e1283828c7ab70fcdbe157
Kacper Piasta <kacperpiasta@gmail.com>
2021-09-29 02:29:18 +0200

    configure gradle
    Signed-off-by: Kacper Piasta <kacperpiasta@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,49 @@
 buildscript {
-    val androidPluginVersion by rootProject.extra { "7.0.2" }
 
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven {
+            setUrl("https://chaquo.com/maven")
+        }
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:$androidPluginVersion")
+        classpath("com.android.tools.build:gradle:7.0.2")
         classpath(kotlin("gradle-plugin", version = "1.5.31"))
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+        classpath("com.chaquo.python:gradle:10.0.1")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.0.0")
+    }
+}
+
+plugins {
+    id("org.jlleitschuh.gradle.ktlint").version("10.2.0").apply(false)
+    id("org.jlleitschuh.gradle.ktlint-idea").version("10.2.0")
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    ktlint {
+        android.set(true)
+        enableExperimentalRules.set(true)
+        outputColorName.set("RED")
+        outputToConsole.set(true)
+    }
+
+    tasks {
+        withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_11.name
+            }
+        }
+
+        withType<org.jlleitschuh.gradle.ktlint.tasks.BaseKtLintCheckTask> {
+            workerMaxHeapSize.set("512m")
+        }
     }
 }
 
