c74ac6ca1108ad20e36927bf1690dee16f172ffb
Kacper Piasta <kacperpiasta@gmail.com>
2021-10-01 00:51:01 +0200

    gradle deps changes
    Signed-off-by: Kacper Piasta <kacperpiasta@gmail.com>

--- app/src/main/kotlin/pl/piasta/coronaradar/ui/main/view/MainActivity.kt
+++ app/src/main/kotlin/pl/piasta/coronaradar/ui/main/view/MainActivity.kt
@@ -1,12 +1,43 @@
 package pl.piasta.coronaradar.ui.main.view
 
+import android.Manifest
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import dagger.hilt.android.AndroidEntryPoint
+import pl.piasta.coronaradar.BuildConfig
 import pl.piasta.coronaradar.ui.main.viewmodel.MainViewModel
+import pl.piasta.coronaradar.utils.ImageUtils
+import java.io.File
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            1
+        )
+        Log.e("tag", BuildConfig.APPLICATION_ID)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            Handler().postDelayed({
+                ImageUtils.generateSpectrogram(File("/sdcard/cough-heavy.wav"), applicationContext)
+            }, 500)
+        }
+    }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.2")
         classpath(kotlin("gradle-plugin", version = "1.5.31"))
+        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
+        classpath("com.google.firebase:perf-plugin:1.4.0")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
         classpath("com.chaquo.python:gradle:10.0.1")
