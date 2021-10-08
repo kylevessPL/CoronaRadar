dcc5b0f39299bbce91a1e981b8443ab133b6d118
Kacper Piasta <kacperpiasta@gmail.com>
2021-10-09 01:59:23 +0200

    add login fragment layout
    Signed-off-by: Kacper Piasta <kacperpiasta@gmail.com>

--- app/src/main/kotlin/pl/piasta/coronaradar/ui/account/AccountActivity.kt
+++ app/src/main/kotlin/pl/piasta/coronaradar/ui/account/AccountActivity.kt
@@ -3,14 +3,18 @@ package pl.piasta.coronaradar.ui.account
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 import pl.piasta.coronaradar.R
 import pl.piasta.coronaradar.databinding.ActivityAccountBinding
 import pl.piasta.coronaradar.ui.base.BaseActivity
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class AccountActivity : BaseActivity<ActivityAccountBinding, Nothing>(R.layout.activity_account) {
 
+    @Inject
+    lateinit var auth: FirebaseAuth
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +22,9 @@ class AccountActivity : BaseActivity<ActivityAccountBinding, Nothing>(R.layout.a
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setupNavController()
+        val action = AccountFragmentDirections.actionAccountFragmentToLoginFragment()
+        if (auth.currentUser == null)
+            navController.navigate(action)
     }
 
     private fun setupNavController() {
