2b99a3cc5592f3ecc43b5cf996e190a3888da0cb
Kacper Piasta <kacperpiasta@gmail.com>
2021-11-12 17:34:15 +0100

    code cleanup
    Signed-off-by: Kacper Piasta <kacperpiasta@gmail.com>

--- app/src/main/python/spectrogram.py
+++ app/src/main/python/spectrogram.py
@@ -6,6 +6,16 @@ from keras_preprocessing import image
 from matplotlib import pyplot as plt
 
 
+def generate_spectrogram(audio_path):
+    img_path = os.path.dirname(audio_path) + '/spec.png'
+    x, sr = librosa.load(audio_path, mono=True, sr=16000)
+    plt.specgram(x, NFFT=2048, Fs=2, Fc=0, noverlap=128, cmap='inferno', sides='default',
+                 mode='default', scale='dB')
+    plt.axis('off')
+    plt.savefig(img_path)
+    return img_path
+
+
 def predict(model_path, test_image):
     interpreter = tflite.Interpreter(model_path)
     interpreter.allocate_tensors()
@@ -19,13 +29,8 @@ def predict(model_path, test_image):
 
 
 def generate(audio_path, model_path):
-    img_file = os.path.dirname(audio_path) + '/spec.png'
-    x, sr = librosa.load(audio_path, mono=True, sr=16000)
-    plt.specgram(x, NFFT=2048, Fs=2, Fc=0, noverlap=128, cmap='inferno', sides='default',
-                 mode='default', scale='dB')
-    plt.axis('off')
-    plt.savefig(img_file)
-    img = image.load_img(img_file, target_size=(640, 480, 3))
+    img_path = generate_spectrogram(audio_path)
+    img = image.load_img(img_path, target_size=(640, 480, 3))
     y = image.img_to_array(img)
     y = y / 255.
     expanded = np.expand_dims(y, axis=0)
