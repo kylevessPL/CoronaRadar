53fc7f434077b500bb4ab8ea8a16e29112dbeec6
Kacper Piasta <kacperpiasta@gmail.com>
2021-11-14 01:46:35 +0100

    publish firebase config
    Signed-off-by: Kacper Piasta <kacperpiasta@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ buildscript {
     }
 
     dependencies {
+        val kotlinVersion = "1.5.31"
+
         classpath("com.android.tools.build:gradle:7.0.3")
         classpath(kotlin("gradle-plugin", version = "1.5.31"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.0-beta02")
@@ -20,6 +22,8 @@ buildscript {
         classpath("com.google.firebase:perf-plugin:1.4.0")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
+        classpath("org.jetbrains.kotlin:kotlin-noarg:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
         classpath("com.chaquo.python:gradle:10.0.1")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.0.0")
     }
