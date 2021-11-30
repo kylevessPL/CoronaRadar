62049ea5e07d88fac26ddee3fca522f217c1374f
Kacper Piasta <kacperpiasta@gmail.com>
2021-11-30 13:40:08 +0100

    keep all gradle plugins version as variable
    Signed-off-by: Kacper Piasta <kacperpiasta@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,19 +13,32 @@ buildscript {
     }
 
     dependencies {
+        val gradleBuildToolsVersion = "7.0.3"
+        val gradlePluginVersion = "1.5.31"
         val kotlinVersion = "1.6.0"
+        val safeArgsVersion = "2.4.0-beta02"
+        val googleServicesVersion = "4.3.10"
+        val firebasePerfPluginVersion = "1.4.0"
+        val firebaseAppDistributionVersion = "2.2.0"
+        val ktLintVersion = "10.2.0"
+        val hiltPluginVersion = "2.40.3"
+        val chaquopyVersion = "10.0.1"
+        val jUnit5PluginVersion = "1.8.0.0"
 
-        classpath("com.android.tools.build:gradle:7.0.3")
-        classpath(kotlin("gradle-plugin", version = "1.5.31"))
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.0-beta02")
-        classpath("com.google.gms:google-services:4.3.10")
-        classpath("com.google.firebase:perf-plugin:1.4.0")
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.3")
+        classpath("com.android.tools.build:gradle:$gradleBuildToolsVersion")
+        classpath(kotlin("gradle-plugin", version = gradlePluginVersion))
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$safeArgsVersion")
+        classpath("com.google.gms:google-services:$googleServicesVersion")
+        classpath("com.google.firebase:perf-plugin:$firebasePerfPluginVersion")
+        classpath(
+            "com.google.firebase:firebase-appdistribution-gradle:$firebaseAppDistributionVersion"
+        )
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:$ktLintVersion")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltPluginVersion")
         classpath("org.jetbrains.kotlin:kotlin-noarg:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
-        classpath("com.chaquo.python:gradle:10.0.1")
-        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.0.0")
+        classpath("com.chaquo.python:gradle:$chaquopyVersion")
+        classpath("de.mannodermaus.gradle.plugins:android-junit5:$jUnit5PluginVersion")
     }
 }
 
