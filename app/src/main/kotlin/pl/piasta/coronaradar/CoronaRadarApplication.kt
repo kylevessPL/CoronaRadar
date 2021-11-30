eed0b7d90f9e5dd92356cfa648f598e9b6ce03d0
Kacper Piasta <kacperpiasta@gmail.com>
2021-11-30 13:27:56 +0100

    fix appcheck verification
    Signed-off-by: Kacper Piasta <kacperpiasta@gmail.com>

--- app/src/main/kotlin/pl/piasta/coronaradar/CoronaRadarApplication.kt
+++ app/src/main/kotlin/pl/piasta/coronaradar/CoronaRadarApplication.kt
@@ -6,6 +6,9 @@ import androidx.core.os.ConfigurationCompat
 import com.akexorcist.localizationactivity.ui.LocalizationApplication
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory
 import dagger.hilt.android.HiltAndroidApp
 import java.util.*
 
@@ -24,10 +27,10 @@ class CoronaRadarApplication : LocalizationApplication() {
     override fun onCreate() {
         super.onCreate()
         Python.start(AndroidPlatform(this))
-//        FirebaseApp.initializeApp(this)
-//        val firebaseAppCheck = FirebaseAppCheck.getInstance()
-//        firebaseAppCheck.installAppCheckProviderFactory(
-//            SafetyNetAppCheckProviderFactory.getInstance()
-//        )
+        FirebaseApp.initializeApp(this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            SafetyNetAppCheckProviderFactory.getInstance()
+        )
     }
 }
